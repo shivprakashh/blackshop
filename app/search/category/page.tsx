045fca9285bbaf24b1b8162ebcd7e14bffb1142c
shivprakashh <shivpraksh518@gmail.com>
2025-04-10 14:14:44 +0630

    secdd

--- app/search/category/page.tsx
+++ app/search/category/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = 'force-dynamic';
 import styles from "../search.module.css"
 import { useSearchParams } from "next/navigation";
 import { useState,useEffect } from "react";
